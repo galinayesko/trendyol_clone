a00e93573b03841388fca9f69c67eb878468898b
galina yesko <galina_yesko@hotmail.com>
2023-01-08 20:35:00 +0300

    tendyol website login page clone with taildwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Source Sans Pro', 'sans-serif'],
+    },
+    },
+  },
+  plugins: [],
+}
